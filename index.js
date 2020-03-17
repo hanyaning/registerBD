87a157dd6d101631927fba86a0b7e684bfd55c8c
hanyaning <1193985098@qq.com>
2020-03-17 18:53:05 +0800

    修改获取验证码处

--- index.js
+++ index.js
@@ -90,8 +90,10 @@ inputVerifyCode.click(function () {
             if (time !== 0) {
                 inputVerifyCode.attr('value', "验证码获取中(" + time + ")");
                 inputVerifyCode.attr("disabled",'disabled');
+                inputVerifyCode.attr("cursor","not-allowed");
             } else {
                 inputVerifyCode.attr('value', "获取验证码");
+                inputVerifyCode.attr("cursor","pointer");
                 userCodeError.html('验证码超时');
                 clearInterval(timer);
             }
